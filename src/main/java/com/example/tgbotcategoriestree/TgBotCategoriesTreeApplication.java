6148fe60c328b4b564c963f429cc20494dac8a43
enovak89 <enovak89@mail.ru>
2023-09-25 17:02:56 +0300

    init commit

---
+++ src/main/java/com/example/tgbotcategoriestree/TgBotCategoriesTreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tgbotcategoriestree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgBotCategoriesTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgBotCategoriesTreeApplication.class, args);
+	}
+
+}
