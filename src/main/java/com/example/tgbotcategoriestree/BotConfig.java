95bb748daafea070834c3113364f87ba2bac3392
enovak89 <enovak89@mail.ru>
2023-10-05 20:29:20 +0300

    upload concept done

--- src/main/java/com/example/tgbotcategoriestree/BotConfig.java
+++ src/main/java/com/example/tgbotcategoriestree/BotConfig.java
@@ -3,4 +3,12 @@ package com.example.tgbotcategoriestree;
 public class BotConfig {
     public static final String BOT_TOKEN = "6459950492:AAHRNR8kIvY5IsXHnSZvijkuUxJGTK119Tg";
     public static final String BOT_NAME = "CategoriesTreeBot";
+
+    /** Method to use in TelegramFileDownloader.class constructor, which needs an Supplier<String> botTokenSupplier
+     *
+     * @return String with bot token
+     */
+    public static String getBotToken(){
+        return BOT_TOKEN;
+    }
 }
--- src/main/java/com/example/tgbotcategoriestree/commands/UploadCommand.java
+++ src/main/java/com/example/tgbotcategoriestree/commands/UploadCommand.java
@@ -1,20 +1,27 @@
 package com.example.tgbotcategoriestree.commands;
 
+import com.example.tgbotcategoriestree.BotConfig;
 import com.example.tgbotcategoriestree.telegramBotsLibraryCustomizedClasses.BotCommandCustom;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.facilities.filedownloader.TelegramFileDownloader;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.Message;
-import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.*;
+import java.net.URL;
+
 @Service
 public class UploadCommand extends BotCommandCustom {
-    public final static boolean UPLOAD_PERMISSION = false;
+    public static boolean UPLOAD_PERMISSION = false;
 
     private final Logger logger = LoggerFactory.getLogger(UploadCommand.class);
 
@@ -23,46 +30,51 @@ public class UploadCommand extends BotCommandCustom {
     }
 
     public void processMessageWithDocument(Message message) {
-        System.out.println("doc ok");
-    }
-
-    @Override
-    public void processMessage(AbsSender absSender, Message message, String[] arguments) {
-        execute(absSender, message.getFrom(), message.getChat(), arguments);
-        System.out.println("doc ok");
-    }
-
-    private boolean checkMessageHasDocAndCorrectCaption(Message message) {
-        try {
-            boolean result = message.hasDocument() && message.getCaption().equals(this.getCommandIdentifier());
-        } catch (NullPointerException e) {
-            SendMessage incorrectCaptureMessage = new SendMessage();
-            incorrectCaptureMessage.setChatId(message.getChatId());
-            incorrectCaptureMessage.setText("Caption is incorrect, please write " + this.getCommandIdentifier()
-                    + " when send the file");
-
-//            try {
-//                execute(incorrectCaptureMessage);
-//            } catch (TelegramApiException exception) {
-//                logger.error(exception.getMessage());
-//            }
+        System.out.println("UPLOAD_PERMISSION from processMessageWithDocument = " + UPLOAD_PERMISSION);
+        if (message.hasDocument() && UPLOAD_PERMISSION) {
+            System.out.println("doc ok");
+            try {
+                URL url = new URL("https://api.telegram.org/bot" + BotConfig.BOT_TOKEN
+                        + "/getFile?file_id=" + message.getDocument().getFileId());
+                BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+                String res = in.readLine();
+                JSONObject jresult = new JSONObject(res);
+                JSONObject path = jresult.getJSONObject("result");
+                String file_path = path.getString("file_path");
+                TelegramFileDownloader fileDownloader = new TelegramFileDownloader(BotConfig::getBotToken);
+                File file = fileDownloader.downloadFile(file_path);
+                Workbook workbook = new XSSFWorkbook(new FileInputStream(file));
+                System.out.println("workbook.getSheetAt(0).getRow(0).getCell(0) = " + workbook.getSheetAt(0).getRow(0).getCell(0));
+                System.out.println("workbook.getSheetAt(0).getRow(0).getCell(1) = " + workbook.getSheetAt(0).getRow(0).getCell(1));
+                UPLOAD_PERMISSION = false;
+            } catch (IOException e) {
+                System.out.println("io ex");
+            } catch (TelegramApiException ex) {
+                System.out.println("API ex");
+            }
+        } else {
+            System.out.println("doc not ok");
         }
-        return true;
     }
-
 
     @Override
     public void execute(AbsSender absSender, User user, Chat chat, String[] arguments) {
         SendMessage getUploadCommandMessage = new SendMessage();
         getUploadCommandMessage.setChatId(chat.getId());
         getUploadCommandMessage.setText("Please, in your response message send XLSX file," +
-                " where put root categories in the first column, its child categories - in the next columns. " +
-                "\nIn message caption write " + this.getCommandIdentifier());
-
+                " where put root categories in the first column, its child categories - in the next columns");
         try {
             absSender.execute(getUploadCommandMessage);
         } catch (TelegramApiException e) {
             logger.error(e.getMessage());
         }
+
+        getUploadCommandMessage.setText("Ready to download");
+        try {
+            absSender.execute(getUploadCommandMessage);
+        } catch (TelegramApiException e) {
+            logger.error(e.getMessage());
+        }
+        UPLOAD_PERMISSION = true;
     }
 }
